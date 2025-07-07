183a352eb6e5ec922a69b65814df9adfc75539d0
Mohsin Ali Ayub <mosin.ayub@outlook.com>
2025-07-08 00:36:46 +0200

    display todo items in table view

--- Tasky/Tasky/View Controllers/ViewController.swift
+++ Tasky/Tasky/View Controllers/ViewController.swift
@@ -12,6 +12,7 @@ class ViewController: UIViewController {
     @IBOutlet private weak var todoItemsTableView: UITableView!
     
     private let cellReuseIdentifier = "TodoItemCell"
+    private var todoItemsManager = TodoItemsManager()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,12 +23,25 @@ class ViewController: UIViewController {
 
 extension ViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 1
+        todoItemsManager.todoItems.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: cellReuseIdentifier, for: indexPath)
+        
+        let item = todoItemsManager.todoItems[indexPath.row]
+        configureCell(cell, for: item)
+        
         return cell
+    }
+    
+    private func configureCell(_ cell: UITableViewCell, for item: TodoItem) {
+        let label = cell.viewWithTag(1000) as! UILabel
+        let imageView = cell.viewWithTag(1001) as! UIImageView
+        
+        label.text = item.text
+        let imageName = item.completed ? "checkmark.circle.fill" : "circle.dotted"
+        imageView.image = UIImage(systemName: imageName)
     }
 }
 

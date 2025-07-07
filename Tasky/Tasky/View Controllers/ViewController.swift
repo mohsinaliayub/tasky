f17d82b21f595753401a4e477efca21971e2506c
Mohsin Ali Ayub <mosin.ayub@outlook.com>
2025-07-08 00:04:45 +0200

    create table view and hook up its delegate, data source and outlet

--- Tasky/Tasky/View Controllers/ViewController.swift
+++ Tasky/Tasky/View Controllers/ViewController.swift
@@ -8,12 +8,29 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet private weak var todoItemsTableView: UITableView!
+    
+    private let cellReuseIdentifier = "TodoItemCell"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
 }
 
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellReuseIdentifier, for: indexPath)
+        return cell
+    }
+}
+
+extension ViewController: UITableViewDelegate {
+    
+}

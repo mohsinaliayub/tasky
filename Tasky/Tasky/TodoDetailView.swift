0a69318a83dceb2a967459a744f7e0c56495fbc3
Mohsin Ali Ayub <mosin.ayub@outlook.com>
2025-12-16 18:54:00 +0100

    add TodoDetail view

---
+++ Tasky/Tasky/TodoDetailView.swift
@@ -0,0 +1,18 @@
+//
+//  TodoDetailView.swift
+//  Tasky
+//
+//  Created by Mohsin Ali Ayub on 16.12.25.
+//
+
+import SwiftUI
+
+struct TodoDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TodoDetailView()
+}

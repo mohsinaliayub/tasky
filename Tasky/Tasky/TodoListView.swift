d023a561de9e5b59995682d1d9c9533d30f0e59f
Mohsin Ali Ayub <mosin.ayub@outlook.com>
2025-12-16 19:03:25 +0100

    create stubs for to-do list view

--- Tasky/Tasky/TodoListView.swift
+++ Tasky/Tasky/TodoListView.swift
@@ -8,14 +8,30 @@
 import SwiftUI
 
 struct TodoListView: View {
+    @State private var todos = [
+        "Brush my teeth",
+        "Buy groceries",
+        "Learn iOS development",
+        "Clean the dishes",
+        "Read one chapter of Harry Potter & the Order of Pheonix"
+    ]
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            ScrollView {
+                VStack(alignment: .leading) {
+                    ForEach(todos, id: \.self) { todo in
+                        Text(todo)
+                            .font(.body)
+                            .kerning(1)
+                            .padding(.vertical, 8)
+                        Divider()
+                    }
+                }
+            }
+            .padding()
+            .navigationTitle("To-dos")
         }
-        .padding()
     }
 }
 
